67cfc7b9355aa9d84eff40219b3d58d6b700bf12
Sumit <bhalgamiyasumit2001@gmail.com>
2026-06-24 10:21:37 +0530

    InfraPortal Project Setup

---
+++ MVCSecurityDemo/Controllers/SecretController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSecurityDemo.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        public ContentResult Secret()
+        {
+            return Content("Secret informations here");
+        }
+
+        [AllowAnonymous]
+        public ContentResult PublicInfo()
+        {
+            return Content("Public informations here");
+        }
+    }
+}
